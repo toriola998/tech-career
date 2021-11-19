91c29422bafcbf0997a26b7ed0c7d6a22a08de0b
Toriola Faidat <faaedah@gmail.com>
2021-11-19 12:09:51 +0100

    added footer styles

---
+++ index.js
@@ -0,0 +1,25 @@
+const menu = document.querySelector('#hamburger');
+const links = document.querySelector('nav');
+const closeIcon = document.querySelector('#close-icon');
+
+//Toggle to menu hamburger
+menu.addEventListener('click', function(){
+links.classList.toggle('show-links');
+});
+
+//Close the side bar once the close icon gets clicked
+closeIcon.addEventListener('click', function(){
+    links.classList.remove('show-links');
+});
+
+let navLinks = document.querySelectorAll('.nav-link');
+//event listener for each links
+navLinks.forEach(link => {  
+  link.addEventListener('click', () => {
+    links.classList.remove('show-links');
+  }); 
+});
+
+//Update the copyright year automatically
+//const date = document.getElementById("date");
+//date.innerHTML = new Date().getFullYear();
