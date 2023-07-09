fd35c55079fd0929c31a6c05db501481599926b0
Toriola <faaedah@gmail.com>
2023-07-09 12:05:22 +0100

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        orange: '#fb5607',
+      }
+    },
+  },
+  plugins: [],
+}
+
